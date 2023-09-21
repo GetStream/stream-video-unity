f75767fbedfcd27955a0aec4590b5aee90909972
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-09-21 14:50:06 +0200

    Add docs client examples

---
+++ Packages/StreamVideo/DocsCodeSamples/01-client-auth/VideoClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StreamVideo.Core;
+using StreamVideo.Libs.Auth;
+using UnityEngine;
+
+namespace StreamVideo.DocsCodeSamples._01_client_auth
+{
+    public class VideoClient : MonoBehaviour
+    {
+        async void Start()
+        {
+            _client = StreamVideoClient.CreateDefaultClient();
+
+            try
+            {
+                var authCredentials = new AuthCredentials("api-key", "user-id", "user-token");
+                await _client.ConnectUserAsync(authCredentials);
+            
+                // After we awaited the ConnectUserAsync the client is connected
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+
+        private IStreamVideoClient _client;
+    }
+}
---
+++ Packages/StreamVideo/DocsCodeSamples/01-client-auth/VideoClientComplete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using StreamVideo.Core;
+using StreamVideo.Core.Configs;
+using StreamVideo.Libs.Auth;
+using UnityEngine;
+
+namespace StreamVideo.DocsCodeSamples._01_client_auth
+{
+    public class VideoClientComplete : MonoBehaviour
+    {
+        async void Start()
+        {
+            var config = new StreamClientConfig
+            {
+                // Enabling Debug level logging can be helpful during development
+                LogLevel = StreamLogLevel.Debug,
+                Audio =
+                {
+                    // Increase audio quality in exchange for higher bandwidth 
+                    EnableRed = false,
+
+                    // DTX encodes silence at lower bitrate. This can save bandwidth
+                    EnableDtx = false
+                }
+            };
+            _client = StreamVideoClient.CreateDefaultClient(config);
+
+            try
+            {
+                var authCredentials = new AuthCredentials("api-key", "user-id", "user-token");
+                await _client.ConnectUserAsync(authCredentials);
+
+                // After we awaited the ConnectUserAsync the client is connected
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+
+        private IStreamVideoClient _client;
+    }
+}

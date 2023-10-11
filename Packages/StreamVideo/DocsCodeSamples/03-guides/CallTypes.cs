b7f8e194835d76f5b3263f10a448fb15d58b910b
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-10-11 12:04:23 +0200

    Add call types page

---
+++ Packages/StreamVideo/DocsCodeSamples/03-guides/CallTypes.cs
@@ -0,0 +1,25 @@
+﻿using StreamVideo.Core;
+
+namespace DocsCodeSamples._03_guides
+{
+    internal class CallTypes
+    {
+        public async void GetOrCreateCall()
+        {
+            var callType = StreamCallType.Default;
+            var callId = "my-call-id";
+
+            var streamCall = await _client.GetOrCreateCallAsync(callType, callId);
+        }
+
+        public async void CreateCallAndJoin()
+        {
+            var callType = StreamCallType.Default;
+            var callId = "my-call-id";
+
+            var streamCall = await _client.JoinCallAsync(callType, callId, create: true, ring: true, notify: false);
+        }
+        
+        private IStreamVideoClient _client;
+    }
+}

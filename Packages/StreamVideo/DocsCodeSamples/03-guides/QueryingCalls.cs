dfefe900d9a6c12ca97fe04ca94d98d929d7fe62
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-10-26 12:01:51 +0200

    [Docs] Add query calls section

--- Packages/StreamVideo/DocsCodeSamples/03-guides/QueryingCalls.cs
+++ Packages/StreamVideo/DocsCodeSamples/03-guides/QueryingCalls.cs
@@ -30,6 +30,35 @@ namespace DocsCodeSamples._03_guides
             // In order to get the previous "page" of results, use this token as a "prev" argument in the QueryCallsAsync method
             var prev = result.Prev;
         }
+        
+        public async void QueryCallsThatWillStartSoon()
+        {
+            // Filter calls that will start in 3 hours and include me
+            var filters = new List<IFieldFilterRule>
+            {
+                CallFilter.StartsAt.LessThanOrEquals(DateTime.Now.AddHours(3)),
+                CallFilter.Members.EqualsTo(_client.LocalUser)
+            };
+
+            // Order them by how soon they start
+            var result = await _client.QueryCallsAsync(filters, CallSort.OrderByAscending(CallSortField.StartsAt), limit: 25);
+        }
+        
+        public async void QueryCallsByCustom()
+        {
+            //StreamTodo: add option to set custom data on IStreamCall
+ 
+            // Filter calls by custom property. For example you can attach a "tag" property to a call and get calls that contain any of the provided tags 
+            var filters = new List<IFieldFilterRule>
+            {
+                CallFilter.Custom("tag").In("xbox", "ps", "switch"),
+            };
+
+            // Order them by how soon they start
+            var sort = CallSort.OrderByAscending(CallSortField.StartsAt);
+
+            var result = await _client.QueryCallsAsync(filters, sort, limit: 25);
+        }
 
         private IStreamVideoClient _client;
     }

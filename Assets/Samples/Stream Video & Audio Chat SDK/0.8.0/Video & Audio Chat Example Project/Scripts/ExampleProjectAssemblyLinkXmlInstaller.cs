226042ef988f2be7e9bd9e2c1f0c236835829ef4
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2025-08-25 15:34:20 +0200

    Fix example project xml installer name

--- Assets/Samples/Stream Video & Audio Chat SDK/0.8.0/Video & Audio Chat Example Project/Scripts/ExampleProjectAssemblyLinkXmlInstaller.cs
+++ Assets/Samples/Stream Video & Audio Chat SDK/0.8.0/Video & Audio Chat Example Project/Scripts/ExampleProjectAssemblyLinkXmlInstaller.cs
@@ -1,12 +1,12 @@
 #if UNITY_EDITOR
 using StreamVideo.Libs.PackageXmlInstaller;
 
-namespace StreamVideo.Libs
+namespace StreamVideo.ExampleProject
 {
     /// <summary> 
-    /// Link Xml installer for StreamVideo.Libs assembly
+    /// Link Xml installer for StreamVideo.ExampleProject assembly
     /// </summary>
-    public class LibsAssemblyLinkXmlInstaller : PackageXmlInstallerBase
+    public class ExampleProjectAssemblyLinkXmlInstaller : PackageXmlInstallerBase
     {
         public override string LinkXmlGuid => "57e429f792d04b47828748874c202c2f";
     }

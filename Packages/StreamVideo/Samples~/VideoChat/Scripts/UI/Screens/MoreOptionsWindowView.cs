e04d95f9c1b4a041baafde99b7af4a29501e3120
Daniel Sierpinski <sierpinski.d@gmail.com>
2025-10-14 09:55:04 +0200

    Sync package samples with the imported samples

---
+++ Packages/StreamVideo/Samples~/VideoChat/Scripts/UI/Screens/MoreOptionsWindowView.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StreamVideo.ExampleProject.UI.Screens
+{
+    public class MoreOptionsWindowView : MonoBehaviour
+    {
+        public void Init(StreamVideoManager streamVideoManager)
+        {
+            _streamVideoManager = streamVideoManager ?? throw new ArgumentNullException(nameof(streamVideoManager));
+            
+            _hideBtn.onClick.AddListener(OnCloseButtonClicked);
+            
+            _muteSelfAudioBtn.onClick.AddListener(() => _streamVideoManager.ActiveCall?.MuteSelf(audio: true, video: false, screenShare: false));
+            _muteOthersAudioBtn.onClick.AddListener(() => _streamVideoManager.ActiveCall?.MuteOthers(audio: true, video: false, screenShare: false));
+            _muteSelfVideoBtn.onClick.AddListener(() => _streamVideoManager.ActiveCall?.MuteSelf(audio: false, video: true, screenShare: false));
+            _muteOthersVideoBtn.onClick.AddListener(() => _streamVideoManager.ActiveCall?.MuteOthers(audio: false, video: true, screenShare: false));
+        }
+        
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+        
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        protected void Awake()
+        {
+            Hide();
+        }
+
+        protected void OnDestroy()
+        {
+            _hideBtn.onClick.RemoveListener(OnCloseButtonClicked);
+        }
+
+        [SerializeField]
+        private Button _muteSelfAudioBtn;
+
+        [SerializeField]
+        private Button _muteOthersAudioBtn;
+        
+        [SerializeField]
+        private Button _muteSelfVideoBtn;
+
+        [SerializeField]
+        private Button _muteOthersVideoBtn;
+
+        [SerializeField]
+        private Button _hideBtn;
+
+        private StreamVideoManager _streamVideoManager;
+
+        private void OnCloseButtonClicked() => Hide();
+    }
+}

a6c469c53cf93b08aca92480ec7782d31a936209
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2025-11-20 21:50:00 +0000

    Release version 0.8.9

--- Packages/StreamVideo/Runtime/Core/LowLevelClient/SdkVersionWrapper.cs
+++ Packages/StreamVideo/Runtime/Core/LowLevelClient/SdkVersionWrapper.cs
@@ -4,6 +4,6 @@ namespace StreamVideo.Core.LowLevelClient
 {
     internal sealed class SdkVersionWrapper
     {
-        public static readonly Version SDKVersion = new Version(0, 8, 8);
+        public static readonly Version SDKVersion = new Version(0, 8, 9);
     }
 }

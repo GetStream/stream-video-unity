988bb7b618702fe7fb028fc79277c3918ef5ad06
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2026-02-27 23:45:02 +0100

    Sync imported samples with package samples

--- Packages/StreamVideo/Samples~/VideoChat/Scripts/UI/ParticipantView.cs
+++ Packages/StreamVideo/Samples~/VideoChat/Scripts/UI/ParticipantView.cs
@@ -22,11 +22,6 @@ namespace StreamVideo.ExampleProject.UI
             }
 
             Participant = participant ?? throw new ArgumentNullException(nameof(participant));
-
-            foreach (var track in Participant.GetTracks())
-            {
-                OnParticipantTrackAdded(Participant, track);
-            }
             
             OnIsSpeakingChanged(Participant.IsSpeaking);
             OnAudioLevelChanged(Participant.AudioLevel);
@@ -167,10 +162,10 @@ namespace StreamVideo.ExampleProject.UI
                 case StreamAudioTrack streamAudioTrack:
                     if (_audioSource != null)
                     {
-                        //StreamTodo: debug why we're sometimes getting multiple audio tracks despite publishing a single audio track
-                        //StreamTodo: handle multiple audio tracks. This is a valid use case
-                        Debug.LogError("Multiple audio track!");
-                        return;
+                        // This can happen after reconnecting.
+                        // We might receive a new track for the same participant after reconnecting
+                        GameObject.Destroy(_audioSource);
+                        _audioSource = null;
                     }
 
                     _audioSource = gameObject.AddComponent<AudioSource>();
